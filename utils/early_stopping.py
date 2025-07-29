a8cb5bdd5a405e0d2c6daf40d5aa9b1db2fe57af
DylanUnifi <dylan.fouepe@edu.unifi.it>
2025-07-29 20:14:30 +0200

    Early stopping Class whose stop the training at the Optimal time

---
+++ utils/early_stopping.py
@@ -0,0 +1,17 @@
+class EarlyStopping:
+    def __init__(self, patience=5, min_delta=0.0):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.best_score = None
+
+    def __call__(self, score):
+        if self.best_score is None or score > self.best_score + self.min_delta:
+            self.best_score = score
+            self.counter = 0
+            return False
+        else:
+            self.counter += 1
+            if self.counter >= self.patience:
+                return True
+            return False
